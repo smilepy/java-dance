911b9d363dadfcc3c7f89a1a3f56377a024c8961
peiy <peiy@xiaopeng.com>
2020-05-30 21:15:57 +0800

    新增JavaDance类

---
+++ src/main/java/com/smilepy/javadance/JavaDance.java
@@ -0,0 +1,11 @@
+package com.smilepy.javadance;
+
+/**
+ * @author peiyu
+ * @date 2020-5-30 21:13
+ */
+public class JavaDance {
+    public static void main(String[] args){
+        System.out.println("hello java-dance!");
+    }
+}
