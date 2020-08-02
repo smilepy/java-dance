8a35c8d834a53be12532c9b55c1d40597c10b466
peiy <peiy@xiaopeng.com>
2020-08-02 14:38:39 +0800

    新增排序算法：递归实现冒泡排序

--- src/test/java/com.smilepy.javadance/util/ArrayUtilsTest.java
+++ src/test/java/com.smilepy.javadance/util/ArrayUtilsTest.java
@@ -2,13 +2,17 @@ package com.smilepy.javadance.util;
 
 import junit.framework.TestCase;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author peiyu
  * @date 2020-5-31 16:41
  */
 public class ArrayUtilsTest extends TestCase {
-    List<String> list=new ArrayList<String>();
+    public void testByte() {
+        int a = 3;
+        int b = (a >> 1);
+        System.out.println(String.format("b:%d", b));
+        int c = 4;
+        int d = (c >> 1);
+        System.out.println(String.format("d:%d", d));
+    }
 }
